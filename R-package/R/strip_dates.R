e2af62d4dcc884dbf3d637224dfb481c0e0d9dcf
Tomislav Hengl <tom.hengl@opengeohub.org>
2020-10-20 15:40:30 +0200

    landcover tiles

--- R-package/R/strip_dates.R
+++ R-package/R/strip_dates.R
@@ -24,9 +24,9 @@ strip_dates <- function(tif.name, year.span=c(2000:2020), type=c("begin", "end")
       x = paste0(year.span[length(year.span)], "-", begin.seasons[5])
     }
   } else {
-    s = which(sapply(sapply(season.name[1:4], function(i){ grep(i, tif.name) }), function(i){length(i)>0}))
+    s = which(sapply(sapply(season.name[1:(length(begin.seasons)-1)], function(i){ grep(i, tif.name) }), function(i){length(i)>0}))
     if(length(s)==0 & type=="begin") { s <- 1 }
-    if(length(s)==0 & type=="end") { s <- 4 }
+    if(length(s)==0 & type=="end") { s <- length(season.name) }
     y = which(sapply(sapply(year.span, function(i){ grep(i, tif.name, fixed = TRUE) }), function(i){length(i)>0}))
     if(length(y)==0 & type=="begin") { y <- year.span[1] }
     if(length(y)==0 & type=="begin") { y <- year.span[length(year.span)] }
