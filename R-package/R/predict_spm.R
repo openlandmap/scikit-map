afa7a2476361577def7e792ae881c42b5987b9bc
Mohammadreza Sheykhmousa <mohammadreza.sheykhmousa@opengeohub.org>
2020-11-09 15:56:54 +0000

    Update predict_spm.R

--- R-package/R/predict_spm.R
+++ R-package/R/predict_spm.R
@@ -18,6 +18,6 @@ predict_spm = function (train_model, newdata){
         }
         vrimp = newdata[1:nrow(newdata),vlp]
         y = predict.variable$response
-        measure_test = predict.variable$score()
-        return(list(y, vrimp,measure_test))
+        #measure_test = predict.variable$score()
+        return(list(y, vrimp))
 }
