fb259c703db0c0a6baa0f11578b3a8fa50420ea3
Tomislav Hengl <tom.hengl@opengeohub.org>
2020-11-30 09:31:39 +0100

    merge

---
+++ eumap.Rcheck/00_pkg_src/eumap/R/predict_spm.R
@@ -0,0 +1,23 @@
+#' predict_spm
+#' @description
+#' prediction on new dataset
+#' @param newdata data at new location,
+#' @param train.model trained model from `train_spm`,
+#' @return 
+#' @export 
+#' @author  \href{https://opengeohub.org/people/mohammadreza-sheykhmousa}{Mohammadreza Sheykhmousa}
+#' @example 
+#' \dontrun{
+#' predict.variable = eumap::predict_spm(train.model, newdata)
+#' }
+
+predict_spm = function (train_model, newdata){
+        predict.variable = train_model(newdata)
+        if(!is.data.frame(newdata)){
+          newdata = as.data.frame(newdata)
+        }
+        vrimp = newdata[1:nrow(newdata),vlp]
+        y = predict.variable$response
+        # measure_test = predict.variable$score()
+        return(list(y, vrimp))
+}
