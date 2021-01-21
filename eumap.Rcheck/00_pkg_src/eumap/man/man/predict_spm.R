fa4e11e83fb81a8c2388a75aa8d9ca02ff965643
Tomislav Hengl <tom.hengl@opengeohub.org>
2021-01-21 17:08:52 +0100

    testing eumap

---
+++ eumap.Rcheck/00_pkg_src/eumap/man/man/predict_spm.R
@@ -0,0 +1,23 @@
+#' Predict_spm
+#' @description
+#' Prediction on a new dataset
+#' @param object Trained model using `train_spm`,
+#' @param newdata Data at new location,
+#' @return Predicted variables, variable importance 
+#' @export 
+#' @author  \href{https://opengeohub.org/people/mohammadreza-sheykhmousa}{Mohammadreza Sheykhmousa}
+#' @examples 
+#' \dontrun{
+#' predict.variable = eumap::predict_spm(object, newdata)
+#' }
+
+predict_spm = function (object, newdata){
+        predict.variable = object(newdata)
+        if(!is.data.frame(newdata)){
+          newdata = as.data.frame(newdata)
+        }
+        vrimp = newdata[1:nrow(newdata),vlp]
+        y = predict.variable$response
+        # measure_test = predict.variable$score()
+        return(list(y, vrimp))
+}
--- eumap.Rcheck/00_pkg_src/eumap/man/man/train_spm.R
+++ eumap.Rcheck/00_pkg_src/eumap/man/man/train_spm.R
@@ -18,6 +18,7 @@
 #' @export 
 #'@author  \href{https://opengeohub.org/people/mohammadreza-sheykhmousa}{Mohammadreza Sheykhmousa} and  \href{https://opengeohub.org/people/tom-hengl}{Tom Hengl}
 #' @examples
+#' \dontrun{ 
 #' ## Meuse Demo
 #' library(sp)
 #' library(mlr3verse)
@@ -57,13 +58,14 @@
 #' #make a spatial prediction map 
 #' plot(df.ts[,"leadp"])
 #' points(meuse, pch="+")
+#' }
 train_spm = function(df.tr, target.variable, parallel = TRUE, predict_type = NULL, folds = NULL, n_evals = NULL, method.list = NULL, var.imp = NULL, super.learner = NULL, crs = NULL, coordinate_names = c("x","y"), ...){
   target = target.variable
-  if ( is.null(predict_type)) {
+  if( is.null(predict_type)){
     predict_type <- "response"
   }
   id = deparse(substitute(df.tr))
-  cv = mlr3::rsmp("repeated_cv", folds = folds)
+  cv = rsmp("repeated_cv", folds = folds)
   task_type = c("classification Task", "Regression Task")
   ml_method = c("kknn", "featureless")
   meta_learner = "ranger"
@@ -74,7 +76,7 @@ train_spm = function(df.tr, target.variable, parallel = TRUE, predict_type = NUL
   ## start running ensemble
   
   ##  classif CV ----
-  if (is.factor(df.tr[,target]) & is.null(crs)) {
+  if(is.factor(df.tr[,target]) & is.null(crs)){
     
     message(paste0(task_type[1],"..."), immediate. = TRUE)
     
@@ -82,7 +84,7 @@ train_spm = function(df.tr, target.variable, parallel = TRUE, predict_type = NUL
     id = id, backend = df.tr, target = target.variable
     )
     
-    ranger_lrn = lrn("classif.ranger", predict_type = "response", importance = "permutation")
+    ranger_lrn = lrn("classif.ranger", predict_type = "response", importance ="permutation")
     ps_ranger = 
       ParamSet$new(
       list(ParamInt$new("mtry", lower = 1L, upper = 5L),
@@ -99,15 +101,11 @@ train_spm = function(df.tr, target.variable, parallel = TRUE, predict_type = NUL
       tuner = tnr("random_search")
       )
       message(resample_method[1], immediate. = TRUE)
-      # at$train(tsk_clf)
-      at$
-        train(tsk_clf)$
-        predict(tsk_clf)$
-        score(msr('classif.acc'))
+      at$train(tsk_clf)
       at$learner$train(tsk_clf)
       best.model = at$archive$best()
       var.imp = at$learner$importance()
-      vlp = names(var.imp[1:(round(length(var.imp)*0.1) + 1)])
+      vlp = names(var.imp[1:(round(length(var.imp)*0.1)+1)])
       summary = at$learner$state$model
       tr.model = at$learner
       train_model = tr.model$predict_newdata
@@ -121,7 +119,7 @@ train_spm = function(df.tr, target.variable, parallel = TRUE, predict_type = NUL
     )   
     
     tsk_regr <- TaskRegr$new(id = id, backend = df.tr, target = target.variable)
-    ranger_lrn = lrn("regr.ranger", predict_type = "response",importance = "permutation")
+    ranger_lrn = lrn("regr.ranger", predict_type = "response",importance ="permutation")
     ps_ranger = ParamSet$new(
       list(
         ParamInt$new("mtry", lower = 1L, upper = 5L),
@@ -140,26 +138,23 @@ train_spm = function(df.tr, target.variable, parallel = TRUE, predict_type = NUL
       )
     #at$store_tuning_instance = TRUE
     message(resample_method[1], immediate. = TRUE)
-    # at$train(tsk_regr)
-    at$
-      train(tsk_regr)$
-      predict(tsk_regr)$
-      score(msr('regr.rmse'))
+    at$train(tsk_regr)
+    
     at$learner$train(tsk_regr)
     tr.model = at$learner
     summary = tr.model$model
     var.imp = tr.model$importance()
-    vlp = names(var.imp[1:(round(length(var.imp)*0.1) + 1)])
+    vlp = names(var.imp[1:(round(length(var.imp)*0.1)+1)])
     train_model = tr.model$predict_newdata
     response = tr.model$model$predictions
   }
   ## classif spcv ----
-  else if (is.factor(df.tr[,target.variable]) & crs == crs) { 
+  else if (is.factor(df.tr[,target.variable]) & crs == crs){ 
     
     message( 
       paste0(task_type[1],"...", immediate. = TRUE)
     ) 
-    if (is.null(method.list) & is.null(super.learner)) {
+    if(is.null(method.list) & is.null(super.learner)){
     method.list <- c("classif.kknn", "classif.featureless", "classif.rpart")
     super.learner = "classif.ranger"
     }
@@ -177,10 +172,11 @@ train_spm = function(df.tr, target.variable, parallel = TRUE, predict_type = NUL
           po("select") %>>% po("learner_cv", id = "kknn", lrn("classif.kknn")),
           po("pca") %>>% po("learner_cv", id = "featureless", lrn("classif.featureless")),
           po("subsample") %>>% po("learner_cv", id = "rpart", lrn("classif.rpart"))
+          
           )
         ) %>>%
       po("featureunion") %>>%
-      po("learner", lrn("classif.ranger",importance = "permutation")) 
+      po("learner", lrn("classif.ranger",importance ="permutation")) 
       resampling_sp = rsmp("repeated_spcv_coords", folds = folds, repeats = 4)
       rr_sp = rsmp(
       task = tsk_regr, learner = g,
@@ -189,25 +185,23 @@ train_spm = function(df.tr, target.variable, parallel = TRUE, predict_type = NUL
       g$keep_results = "TRUE"
       # plt = g$plot()
       message(resample_method[2], immediate. = TRUE)
-      g$
-        train(tsk_clf)$
-        predict(tsk_clf)$
-        score(msr('classif.acc'))
+      g$train(tsk_clf)
+      g$predict(tsk_clf)
       conf.mat = g$pipeops$classif.ranger$learner_model$model$confusion.matrix
       var.imp = g$pipeops$classif.ranger$learner_model$model$variable.importance
-      vlp = names(var.imp[1:(round(length(var.imp)*0.1) + 1)])
+      vlp = names(var.imp[1:(round(length(var.imp)*0.1)+1)])
       summary = g$pipeops$classif.ranger$learner_model$model
       tr.model = g$pipeops$classif.ranger$learner$train(tsk_clf)
       train_model = tr.model$predict_newdata
       response = tr.model$model$predictions
   }
   ## regr spcv ----
-  else if (is.numeric(df.tr[,target.variable]) & crs == crs) {
+  else if(is.numeric(df.tr[,target.variable]) & crs == crs){
     
     message( 
     paste0(task_type[2],"...", immediate. = TRUE)
     ) 
-    if (is.null(method.list) & is.null(super.learner)) {
+    if(is.null(method.list) & is.null(super.learner)){
       method.list <- c("regr.kknn", "regr.featureless", "regr.rpart")
       super.learner <- "regr.ranger"
     }
@@ -233,7 +227,7 @@ train_spm = function(df.tr, target.variable, parallel = TRUE, predict_type = NUL
           )
         ) %>>%
       po("featureunion") %>>%
-      po("learner", lrn("regr.ranger",importance = "permutation"))
+      po("learner", lrn("regr.ranger",importance ="permutation"))
     
     resampling_sp = rsmp("repeated_spcv_coords", folds = folds, repeats = 3)
     rr_sp = rsmp(
@@ -243,14 +237,12 @@ train_spm = function(df.tr, target.variable, parallel = TRUE, predict_type = NUL
     g$keep_results = "TRUE"
     plt = g$plot()
     message(resample_method[2], immediate. = TRUE)
-    g$
-      train(tsk_regr)$
-      predict(tsk_regr)$
-      score(msr('regr.rmse'))
+    g$train(tsk_regr)
+    g$predict(tsk_regr)
     summary = g$pipeops$regr.ranger$learner_model$model
     tr.model = g$pipeops$regr.ranger$learner$train(tsk_regr)
     var.imp = tr.model$importance()
-    vlp = names(var.imp[1:(round(length(var.imp)*0.1) + 1)])
+    vlp = names(var.imp[1:(round(length(var.imp)*0.1)+1)])
     response = tr.model$model$predictions
     train_model = tr.model$predict_newdata
     response = tr.model$model$predictions
