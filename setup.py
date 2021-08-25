102a3b0ecd4058d9e5d67a3dd6a1f80858a28348
Leandro Parente <leal.parente@gmail.com>
2021-08-25 11:58:26 +0200

    Fix for documentation build.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     ],
     python_requires='>=3.7',
     install_requires=[
-        'GDAL>=3.1',
+        'GDAL',
         'affine>=2.3',
         'geopandas>=0.8',
         'joblib>=0.17',
