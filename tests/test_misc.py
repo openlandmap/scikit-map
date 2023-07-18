38ead76a667a1cacfec2d7bd583b3394ccd7305d
Leandro Parente <leal.parente@gmail.com>
2023-07-18 16:46:38 +0200

    refactor: rename method and class names for TestDataRange

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -2,9 +2,9 @@ import pytest
 from skmap.misc import date_range
 from datetime import datetime
 
-class TestDataRange:
+class TestDateRange:
 
-  def pluck_year(self, dates, sep = '-'):
+  def _pluck_year(self, dates, sep = '-'):
     
     result = []
     
@@ -17,77 +17,77 @@ class TestDataRange:
     return sep.join(result)
 
   def test_001(self):
-    assert self.pluck_year(
+    assert self._pluck_year(
       date_range('2013-01-01', '2016-01-01', 'months', 1, 
         ignore_29feb=True)
-    ) == self.pluck_year(
+    ) == self._pluck_year(
       date_range('2016-01-01', '2019-01-01', 'months', 1, 
         ignore_29feb=True)
     )
 
   def test_002(self):
-    assert self.pluck_year(
+    assert self._pluck_year(
       date_range('2013-01-01', '2016-01-01', 'months', 1, 
         ignore_29feb=True)
-    ) != self.pluck_year(
+    ) != self._pluck_year(
       date_range('2016-01-01', '2019-01-01', 'months', 1,
        ignore_29feb=False)
     )
 
   def test_003(self):
-    assert self.pluck_year(
+    assert self._pluck_year(
       date_range('2013001', '2016001', 'months', 1, 
         date_format="%Y%j", ignore_29feb=True)
-    ) == self.pluck_year(
+    ) == self._pluck_year(
       date_range('2016001', '2019001', 'months', 1, 
         date_format="%Y%j", ignore_29feb=True)
     )
 
   def test_004(self):
-    assert self.pluck_year(
+    assert self._pluck_year(
       date_range('2013001', '2016001', 'months', 1, 
         date_format="%Y%j", ignore_29feb=True)
-    ) != self.pluck_year(
+    ) != self._pluck_year(
       date_range('2016001', '2019001', 'months', 1, 
         date_format="%Y%j", ignore_29feb=False)
     )
 
   def test_005(self):
     date_step = ([16] * 22) + [13]
-    assert self.pluck_year(
+    assert self._pluck_year(
       date_range('2013001', '2016001', 'days', date_step, 
         date_format="%Y%j", ignore_29feb=True)
-    ) == self.pluck_year(
+    ) == self._pluck_year(
       date_range('2016001', '2019001', 'days', date_step, 
         date_format="%Y%j", ignore_29feb=True)
     )
 
   def test_006(self):
     date_step = ([16] * 22) + [13]
-    assert self.pluck_year(
+    assert self._pluck_year(
       date_range('2013001', '2016001', 'days', date_step, 
         date_format="%Y%j", ignore_29feb=True)
-    ) != self.pluck_year(
+    ) != self._pluck_year(
       date_range('2016001', '2019001', 'days', date_step, 
         date_format="%Y%j", ignore_29feb=False)
     )
 
   def test_007(self):
     date_step = ([16] * 22) + [13]
-    assert self.pluck_year(
+    assert self._pluck_year(
       date_range('2013001', '2016001', 'days', date_step, 
         date_format="%Y%j", ignore_29feb=True, return_str=True)
-    ) == self.pluck_year(
+    ) == self._pluck_year(
       date_range('2016001', '2019001', 'days', date_step, 
         date_format="%Y%j", ignore_29feb=True, return_str=True)
     )
 
   def test_008(self):
     date_step = ([16] * 22) + [13]
-    assert self.pluck_year(
+    assert self._pluck_year(
       date_range('2013001', '2016001', 'days', date_step, 
         date_format="%Y%j", ignore_29feb=True, return_str=True)
-    ) != self.pluck_year(
+    ) != self._pluck_year(
       date_range('2016001', '2019001', 'days', date_step, 
         date_format="%Y%j", ignore_29feb=False, return_str=True)
     )
