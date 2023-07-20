814808757bb6096ab63eedd162c4f0abcd141943
Leandro Parente <leal.parente@gmail.com>
2023-07-20 13:54:24 +0200

    feat: add swir1 and dtm static to toy raster data

--- skmap/data/toy.py
+++ skmap/data/toy.py
@@ -7,25 +7,40 @@ from skmap.misc import find_files
 from skmap.io import read_rasters, RasterData
 
 BASE_DIR = Path(__file__).parent
-DATA_DIR = BASE_DIR.joinpath('data')
+DATA_DIR = BASE_DIR.joinpath('toy')
+TOY_DATE_STEP = [109, 96, 80, 80]
 
-def ndvi_files(gappy=False):
+def _static_raster():
+  return find_files(DATA_DIR.joinpath('stic'), '*.tif')
+
+def _temporal_raster(type, subpath = None):
+  
+  raster_dir = DATA_DIR.joinpath(type)
+  if subpath is not None:
+    raster_dir = raster_dir.joinpath(subpath)
+  
+  base_dt = '20200913_20201201'
+  
+  raster_files = find_files(raster_dir, f'*{base_dt}*.tif')
+  return str(raster_files[0]).replace(base_dt,'{dt}')
+
+def rdata(verbose=True):
+  return RasterData({
+    'ndvi': _temporal_raster('ndvi', 'filled'),
+    'swir1': _temporal_raster('swir1'),
+    'static': _temporal_raster('swir1')
+    }, verbose=verbose
+  ).timespan('20141202', '20201201', 'days', 
+    TOY_DATE_STEP, ignore_29feb=True
+  ).read()
+
+def ndvi_rdata(gappy=False, verbose=True):
   subpath = 'gappy' if gappy else 'filled'
-  return find_files(DATA_DIR.joinpath('ndvi').joinpath(subpath), '*.tif')
-
-def ndvi_data(gappy=False, return_array = False, verbose=True):
-  if return_array == False:
-    subpath = 'gappy' if gappy else 'filled'
-    base_dt = '20200913_20201201'
-    ndvi_files = find_files(DATA_DIR.joinpath('ndvi').joinpath(subpath), f'*{base_dt}*.tif')
-    ndvi_files = str(ndvi_files[0]).replace(base_dt,'{dt}')
-    return RasterData(
-              ndvi_files, verbose=verbose
-            ).timespan('20141202', '20201201', 'days', 
-              [109, 96, 80, 80], ignore_29feb=True
-            ).read()
-  else:
-    return RasterData(ndvi_files(gappy))
+  return RasterData( _temporal_raster('ndvi', subpath),
+    verbose=verbose
+  ).timespan('20141202', '20201201', 'days', 
+    TOY_DATE_STEP, ignore_29feb=True
+  ).read()
 
 def lc_samples():
   DATA_DIR.joinpath('lc').joinpath('samples.gpkg')
