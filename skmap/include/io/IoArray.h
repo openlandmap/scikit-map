ba5c987a3c8a6d8ee01ff8bd95d7de7ca456131e
Davide Consoli <davide.consoli@opengeohub.org>
2024-02-13 18:20:52 +0000

    Add S3 pushing in C++ backend

--- skmap/include/io/IoArray.h
+++ skmap/include/io/IoArray.h
@@ -44,7 +44,8 @@ class IoArray: public ParArray
                         uint_t y_size,
                         GDALDataType write_type,
                         T no_data_value,
-                        std::string bash_compression_command)
+                        std::optional<std::string> bash_compression_command,
+                        std::optional<std::vector<std::string>> seaweed_path)
         {
             skmapAssertIfTrue(m_data.cols() < x_size * y_size, "scikit-map ERROR 9: reading region size smaller then the number of columns");
             GDALDataset *inputDataset = (GDALDataset *)GDALOpen(base_file.c_str(), GA_ReadOnly);
@@ -52,12 +53,18 @@ class IoArray: public ParArray
             inputDataset->GetGeoTransform(geotransform);
             auto projection = inputDataset->GetProjectionRef();
             auto spatial_ref = inputDataset->GetSpatialRef();
+            std::string ending;
+            if (bash_compression_command.has_value())
+                ending = "_tmp.tif";
+            else 
+                ending = ".tif";
+
             auto writeTiff = [&] (uint_t i, Eigen::Ref<MatFloat::RowXpr> row)
             {
                 std::string layer_name = layer_names[i];
                 GDALDriver *driver = GetGDALDriverManager()->GetDriverByName("GTiff");
                 Eigen::RowVectorX<T> casted_row = row.cast<T>();
-                GDALDataset *writeDataset = driver->Create((base_folder + "/" + layer_name + "_tmp.tif").c_str(),
+                GDALDataset *writeDataset = driver->Create((base_folder + "/" + layer_name + ending).c_str(),
                     inputDataset->GetRasterXSize(), inputDataset->GetRasterYSize(), 1, write_type, nullptr);
                 writeDataset->SetGeoTransform(geotransform);
                 writeDataset->SetSpatialRef(spatial_ref);
@@ -70,9 +77,18 @@ class IoArray: public ParArray
                 skmapAssertIfTrue(out_write != CE_None,
                    "scikit-map ERROR 11: issues in writing the file " + layer_name);
                 GDALClose(writeDataset);
-                runBashCommand(bash_compression_command + " " + base_folder + "/" + layer_name + "_tmp.tif "
-                             + base_folder + "/" + layer_name + ".tif");
-                runBashCommand("rm " + base_folder + "/" + layer_name + "_tmp.tif");
+                if (bash_compression_command.has_value())
+                {                    
+                    runBashCommand(bash_compression_command.value() + " " + base_folder + "/" + layer_name + ending + " "
+                                 + base_folder + "/" + layer_name + ".tif");
+                    runBashCommand("rm " + base_folder + "/" + layer_name + ending);
+                }
+                if (seaweed_path.has_value())
+                {                    
+                    runBashCommand("mc cp " + base_folder + "/" + layer_name + ".tif " +
+                                              seaweed_path.value()[i] + "/" + layer_name + ".tif ");
+                    runBashCommand("rm " + base_folder + "/" + layer_name + ".tif");
+                }
 
             };
             this->parRowPerm(writeTiff, data_indices);
