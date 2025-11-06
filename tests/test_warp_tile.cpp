18b85e2cd3bbc81ad593db0894d51ca55de69154
d-consoli <davide.consoli@opengeohub.org>
2025-11-06 15:44:20 +0100

    Add data source URL in test_warp_tile.cpp
    Added a URL for the data source in the main function.

--- tests/test_warp_tile.cpp
+++ tests/test_warp_tile.cpp
@@ -3,6 +3,7 @@ using namespace skmap;
 
 void main()
 {
+    "https://s3.openlandmap.org/arco/wv_mcd19a2v061.seasconv.sd.yearly_p50_1km_s_20000101_20001231_go_epsg.4326_v20230619.tif"
     IoArray ioArray(data, n_threads);
     ioArray.setupGdal(convPyDict(conf_GDAL));
     ioArray.warpTile(tilePath, mosaicPath, resample);
