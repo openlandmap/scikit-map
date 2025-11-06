67ec0118e8c89cc693daf94535b0211269919039
d-consoli <davide.consoli@opengeohub.org>
2025-11-06 13:48:34 +0100

    Update test_warp_tile.cpp

--- tests/test_warp_tile.cpp
+++ tests/test_warp_tile.cpp
@@ -1,1 +1,9 @@
+#include "io/IoArray.h"
+using namespace skmap;
 
+void main()
+{
+    IoArray ioArray(data, n_threads);
+    ioArray.setupGdal(convPyDict(conf_GDAL));
+    ioArray.warpTile(tilePath, mosaicPath, resample);
+}
